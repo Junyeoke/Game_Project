7e07f235be2d0f177f6c0f2fa72e59240766a055
StarsinLiver <141594965+StarsinLiver@users.noreply.github.com>
2023-12-03 13:15:41 +0900

    sdsa

--- frontend-typescript/src/utils/http-common.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  // baseURL: "http://43.201.18.3:8000/api",
-  baseURL: "http://localhost:8000/api",
-  headers: {
-    "Content-Type": "application/json",
-  }
-});
